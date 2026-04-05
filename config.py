2568d8d4b7a3eb099d67298395211a6726fb7275
hema7324 <hema.24bca7324@vitap.ac.in>
2026-04-05 22:33:04 +0530

    updateddb to railway

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
-DB_HOST = os.environ.get("DB_HOST", "localhost")
-DB_PORT = int(os.environ.get("DB_PORT", 3306))
-DB_USER = os.environ.get("DB_USER", "root")
-DB_PASSWORD = os.environ.get("DB_PASSWORD", "1234") # Change this to your actual MySQL root password
+DB_HOST = "mysql.railway.internal"
+DB_PORT = 3306
+DB_USER = "root"
+DB_PASSWORD = "yvRTAyOLyelwKOeSoHlQKkeokigdhRKa" # Change this to your actual MySQL root password
 
 # Database names
 USERS_DB = "lms_users_db"
