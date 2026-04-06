041d7446f89128b62c01634b14458368c7e93627
hema7324 <hema.24bca7324@vitap.ac.in>
2026-04-06 11:28:38 +0530

    Fixed db connection railwy

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
 DB_HOST = "mysql.railway.internal"
-DB_PORT = 3306
+DB_PORT = 3306 #os.getenv("DB_PORT", 3306)  # Default MySQL port
 DB_USER = "root"
 DB_PASSWORD = "BdYhWPwOKWXRExXunZXfRwlJYffLYkzZ" # Change this to your actual MySQL root password
 
 # Database names
-USERS_DB = "lms_users_db"
-BOOKS_DB = "lms_books_db"
+USERS_DB = "db1"
+BOOKS_DB = "db2"
--- main.py
+++ main.py
@@ -169,4 +169,4 @@ def serve_index():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
